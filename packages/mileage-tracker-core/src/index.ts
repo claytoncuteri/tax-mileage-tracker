e990c64e36670209f936a0113766fea5f0550bfa
claytoncuteri <cuteri.clayton28@gmail.com>
2026-03-09 09:45:00 -0500

    chore: scaffold mileage-tracker-core package
    Set up Vite library mode build with ESM + CJS output, TypeScript strict
    mode, and peer dependency on React 18+.

---
+++ packages/mileage-tracker-core/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Mileage Tracker Core
+ *
+ * IRS-compliant mileage tracking React component library.
+ * Track business vs personal vehicle mileage for tax deductions.
+ */
+
+// Types will be exported here as they are built
+export {};
